612db8a75ab8fd90541f5792cd68892761ca3054
Java sauce <mohammedbrandon204@gmail.com>
2022-09-08 16:14:23 -0400

    tools

---
+++ item/GummyBearPickaxeItem.java
@@ -0,0 +1,67 @@
+
+package net.mcreator.willywonka.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.item.PickaxeItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.IItemTier;
+
+import net.mcreator.willywonka.WillywonkaModElements;
+
+@WillywonkaModElements.ModElement.Tag
+public class GummyBearPickaxeItem extends WillywonkaModElements.ModElement {
+	@ObjectHolder("willywonka:gummy_bear_pickaxe")
+	public static final Item block = null;
+
+	public GummyBearPickaxeItem(WillywonkaModElements instance) {
+		super(instance, 28);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new PickaxeItem(new IItemTier() {
+			public int getMaxUses() {
+				return 1000;
+			}
+
+			public float getEfficiency() {
+				return 10f;
+			}
+
+			public float getAttackDamage() {
+				return 2f;
+			}
+
+			public int getHarvestLevel() {
+				return 1;
+			}
+
+			public int getEnchantability() {
+				return 2;
+			}
+
+			public Ingredient getRepairMaterial() {
+				return Ingredient.EMPTY;
+			}
+		}, 1, 4f, new Item.Properties().group(ItemGroup.TOOLS)) {
+			@Override
+			public boolean hasContainerItem() {
+				return true;
+			}
+
+			@Override
+			public ItemStack getContainerItem(ItemStack itemstack) {
+				return new ItemStack(this);
+			}
+
+			@Override
+			public boolean isRepairable(ItemStack itemstack) {
+				return false;
+			}
+		}.setRegistryName("gummy_bear_pickaxe"));
+	}
+}
